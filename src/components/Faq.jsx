ce2c5cb8447125813c3551bac2dd35352a4d71d4
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-11-05 21:33:59 +0600

    added faq section

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -8,46 +8,55 @@ const Faq = () => {
       <div className="collapse text-white collapse-arrow join-item border border-base-300">
         <input type="radio" name="my-accordion-4" checked="checked" />
         <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
+        What is an online group study website?
         </div>
         <div className="collapse-content">
-          <p>hello</p>
+          <p>An online group study website is a platform that facilitates collaborative learning and study sessions over the internet. It allows users to create or join study groups, connect with fellow students, and study together in a virtual environment.</p>
         </div>
       </div>
       <div className="collapse text-white collapse-arrow join-item border border-base-300">
         <input type="radio" name="my-accordion-4" />
         <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
+        How can I benefit from using an online group study website?
         </div>
         <div className="collapse-content">
-          <p>hello</p>
+          <p> Online group study websites offer several benefits, including access to study resources, the ability to connect with peers, share knowledge, and work on assignments or projects together. It can also help improve motivation, accountability, and productivity in your studies.</p>
         </div>
       </div>
       <div className="collapse text-white collapse-arrow join-item border border-base-300">
         <input type="radio" name="my-accordion-4" />
         <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
+        Are there any costs associated with using an online group study website?
         </div>
         <div className="collapse-content">
-          <p>hello</p>
+          <p> It depends on the platform. Some online group study websites offer free basic features, while others may have premium or subscription-based plans with additional benefits. Always check the pricing structure of the specific website you are interested in.</p>
         </div>
       </div>
       <div className="collapse text-white collapse-arrow join-item border border-base-300">
         <input type="radio" name="my-accordion-4" />
         <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
+        Can I use these websites for different subjects and levels of education?
         </div>
         <div className="collapse-content">
-          <p>hello</p>
+          <p>Yes, many online group study websites cater to a wide range of subjects and educational levels, from high school to college and beyond. You can often find study groups for various subjects and courses.</p>
         </div>
       </div>
       <div className="collapse text-white collapse-arrow join-item border border-base-300">
         <input type="radio" name="my-accordion-4" />
         <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
+        How can online group study websites improve my study habits?
         </div>
         <div className="collapse-content">
-          <p>hello</p>
+          <p> Online group study websites can enhance your study habits by providing a structured environment for learning, encouraging accountability, promoting collaborative learning, and offering access to study materials and resources that can aid your comprehension and retention of the subject matter.
+
+Q: Are there any tips for making the most of an online group study website?
+A: To maximize the benefits, it is important to actively participate, set goals, communicate with your group, stay organized, and be consistent in your study sessions. Additionally, you can leverage the resources and features offered by the platform, such as shared documents and discussion boards.
+
+
+
+
+
+</p>
         </div>
       </div>
     </div>
